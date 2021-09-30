fc68b043a98561f5d47e27b6bfa1e2851d10ecae
cryptoinhero <33366277+cryptoinhero@users.noreply.github.com>
2021-10-01 01:14:01 +0800

    init commit

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,190 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://exchange.momoprotocol.finance/",
+      },
+      {
+        label: "Liquidity",
+        href: "https://exchange.momoprotocol.finance/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Staking",
+    icon: "PoolIcon",
+    href: "/staking",
+  },
+  // {
+  //   label: "Lottery",
+  //   icon: "TicketIcon",
+  //   href: "/lottery",
+  // },
+  // {
+  //   label: "NFT",
+  //   icon: "NftIcon",
+  //   href: "/nft",
+  // },
+  // {
+  //   label: "Profile & Teams",
+  //   icon: "GroupsIcon",
+  //   items: [
+  //     {
+  //       label: "Leaderboard",
+  //       href: "/",
+  //     },
+  //     {
+  //       label: "YourProfile",
+  //       href: "/",
+  //     },
+  //   ],
+  //   calloutClass: "rainbow",
+  // },
+  // {
+  //   label: "Info",
+  //   icon: "InfoIcon",
+  //   items: [
+  //     {
+  //       label: "Overview",
+  //       href: "https://pancakeswap.info",
+  //     },
+  //     {
+  //       label: "Tokens",
+  //       href: "https://pancakeswap.info/tokens",
+  //     },
+  //     {
+  //       label: "Pairs",
+  //       href: "https://pancakeswap.info/pairs",
+  //     },
+  //     {
+  //       label: "Accounts",
+  //       href: "https://pancakeswap.info/accounts",
+  //     },
+  //   ],
+  // },
+  {
+    label: "IDO",
+    icon: "IfoIcon",
+    items: [
+      {
+        label: "Next",
+        href: "/ido",
+      },
+      {
+        label: "History",
+        href: "/ifo/history",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      // {
+      //   label: "Voting",
+      //   href: "https://voting.pancakeswap.finance",
+      // },
+      {
+        label: "Github",
+        href: "https://github.com/MomoProtocol762",
+      },
+      {
+        label: "Docs",
+        href: "https://momoprotocol-1.gitbook.io/momoprotocol/",
+      },
+      // {
+      //   label: "Blog",
+      //   href: "https://goosefinance.medium.com/",
+      // },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "Chat",
+        href: "https://t.me/momoprotocol_chat",
+      },
+      {
+        label: "ANN",
+        href: "https://t.me/momoprotocol",
+      },
+      // {
+      //   label: "中文",
+      //   href: "https://t.me/momofinancechinese",
+      // },
+      // {
+      //   label: "Tiếng Việt",
+      //   href: "https://t.me/PancakeSwapVN",
+      // },
+      // {
+      //   label: "Italiano",
+      //   href: "https://t.me/momofinanceitalian",
+      // },
+      // {
+      //   label: "русский",
+      //   href: "https://t.me/momofinancerussian",
+      // },
+      // {
+      //   label: "Türkiye",
+      //   href: "https://t.me/momoFinanceTurkey",
+      // },
+      // {
+      //   label: "Português",
+      //   href: "https://t.me/PancakeSwapPortuguese",
+      // },
+      // {
+      //   label: "Español",
+      //   href: "https://t.me/momofinancespanish",
+      // },
+      // {
+      //   label: "日本語",
+      //   href: "https://t.me/momofinancejapanese",
+      // },
+      // {
+      //   label: "Français",
+      //   href: "https://t.me/momofinancefrench",
+      // },
+      // {
+      //   label: "Announcements",
+      //   href: "https://t.me/momofinanceann",
+      // },
+      // {
+      //   label: "Price Bot",
+      //   href: "https://t.me/BinanceRocketEGG",
+      // },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/momo_protocol",
+  },
+  // {
+  //   label: "Reddit",
+  //   icon: "RedditIcon",
+  //   href: "https://www.reddit.com/r/MomoFinanceofficial",
+  // },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
